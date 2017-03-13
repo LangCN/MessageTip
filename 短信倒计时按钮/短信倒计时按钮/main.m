c6d66b6705deced2baf14a247f229b919a54a331
lin <1506202164@qq.com>
2017-03-13 19:55:49 +0800

    常用的短信倒计时工具

---
+++ 短信倒计时按钮/短信倒计时按钮/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  短信倒计时按钮
+//
+//  Created by linchao on 17/3/13.
+//  Copyright © 2017年 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
